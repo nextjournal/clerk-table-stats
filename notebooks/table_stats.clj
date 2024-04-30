8172b807ccc15e0fa6ff36e7fa0a7baeb370fd14
Michiel Borkent <michielborkent@gmail.com>
2024-04-30 16:07:02 +0200

    wip

--- notebooks/table_stats.clj
+++ notebooks/table_stats.clj
@@ -19,6 +19,31 @@
   (let [_run-at #inst "2021-05-20T08:28:29.445-00:00"
         ds (jdbc/get-datasource {:dbtype "sqlite" :dbname "chinook.db"})]
     (with-open [conn (jdbc/get-connection ds)]
-      (clerk/table (jdbc/execute! conn (sql/format {:select [:AlbumId :Bytes :Name :TrackID
+      (clerk/table (jdbc/execute! conn (sql/format {:select [:albums.title :Bytes :Name :TrackID
                                                              :UnitPrice]
-                                                    :from :tracks}))))))
+                                                    :from :tracks
+                                                    :join [:albums [:= :tracks.AlbumId :albums.AlbumId]]}))))))
+
+(def nested-seq-of-map
+  [{:ars/id "1"
+    :compound/name "Krefeld"
+    :ductile/id #uuid "1174774f-17ec-442c-803f-2906015be68f"
+    :entry/datetime #inst "2023-09-28T06:33:01Z"
+    :entry/transport {:transport/mode :mode/truck
+                      :transport/name "Kempers"}
+    :exit/transport {:transport/mode :mode/truck
+                     :transport/name "Kempers"}}
+   {:ars/id "2"
+    :compound/name "Krefeld"
+    :ductile/id #uuid "774f1174-7ec1-2c44-3f80-15be68f29060"
+    :entry/transport {:transport/mode :mode/truck
+                      :transport/name "Kempers"}
+    :exit/transport {:transport/mode :mode/truck}}])
+
+(clerk/table {::clerk/render-opts {:group-headers true
+                                   :column-order [:compound/name
+                                                  [:entry/transport [:transport/name :transport/mode]]
+                                                  [:exit/transport [:transport/name :transport/mode]]
+                                                  :entry/datetime]
+                                   :hide-columns [:ars/id :ductile/id]}} nested-seq-of-map)
+
