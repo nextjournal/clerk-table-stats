709275cd4ed8b3a6f4c935e5a3d25fb18c73fbab
Philippa Markovics <philippa@markovics.com>
2024-08-13 15:16:03 +0200

    Add table-filters notebook

---
+++ notebooks/table_filters.clj
@@ -0,0 +1,16 @@
+;; # Table Filters
+
+(ns ^:nextjournal.clerk/no-cache table-filters
+  (:require [nextjournal.clerk :as clerk]
+            [nextjournal.clerk-table-stats :as clerk-table-stats]))
+
+(clerk/with-viewer clerk-table-stats/viewer
+  {::clerk/render-opts {:filters true}}
+  [{:category :bang :value 11}
+   {:category :barx :value 20}
+   {:category :bang :value 10}
+   {:category :barx :value 15}
+   {:category :barx :value 22}
+   {:category :bug :value 12}
+   {:category :bug :value 22}])
+
