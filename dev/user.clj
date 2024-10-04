41e075279abe78bad54f7e7d3be18054f3b63eeb
Martin Kavalar <martin@nextjournal.com>
2024-10-04 13:09:25 +0200

    Fix cider-jack-in

--- dev/user.clj
+++ dev/user.clj
@@ -1,4 +1,7 @@
 (ns user
   (:require [nextjournal.clerk :as clerk]))
 
-(clerk/serve! {:port 7890 :render-nrepl {}})
+(future
+  (Thread/sleep 1000)
+  (clerk/show! "notebooks/table_filters.clj")
+  (clerk/serve! {:port 7890 :render-nrepl {} :browse true}))
